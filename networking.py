1fa0522efceef55a96ff5bc2d0fcfc469df263ff
Sheil-Patel <59850027+Sheil-Patel@users.noreply.github.com>
2020-04-14 00:14:10 -0400

    Start Project

--- networking.py
+++ networking.py
@@ -1,3 +1,17 @@
 #Networking-Application/networking.py
 
 #TODO:
+
+import os
+
+def get_first_name():
+    name = input("Please input your first name\n")
+    return name
+
+
+
+
+if __name__ == "__main__":
+    
+    name = get_first_name()
+    print(name)
